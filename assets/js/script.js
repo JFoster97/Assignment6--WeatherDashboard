40433f0a608675d6dc0dd4dfacc7b698419b45bb
Jonah Foster <jonahfoster97@gmail.com>
2022-03-23 08:38:00 -0500

    created a function to pull information from previously searched cities

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+var apiKey = "1c5a8875c0ef6e356265df7b0a88878a";
+
 var searchFormEl = document.querySelector('#search-form');
 
 function handleSearchFormSubmit(event) {
@@ -7,6 +9,7 @@ function handleSearchFormSubmit(event) {
   var formatInputVal = document.querySelector('#format-input').value;
 
   if (!searchInputVal) {
+    alert('please search for a city')
     console.error('You need a search input value!');
     return;
   }
@@ -17,3 +20,30 @@ function handleSearchFormSubmit(event) {
 }
 
 searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+
+function listForecast(city){
+  var apiCallURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+  $.ajax({
+      url: apiCallURL,
+      method: "GET"
+  }).then(function(response){
+      var weatherIcon = response.weather[0].icon;
+      var date = $("<h2>").text(moment().format('l'));
+      var icon = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png"); 
+      var farenheit = (response.main.temp - 273.15) * 1.80 + 32;
+
+      $("mainCityName").text(response.name);
+      $("mainCityName").append(date);
+      $("mainCityName").append(icon);
+      $("mainCityTemp").text(farenheit.toFixed(2) + " \u00B0F");
+      $("mainCityHumidity").text(response.main.humidity + "%");
+      $("mainCityWind").text(response.wind.speed + "MPH");
+
+      $.ajax({
+        url: apiCallURL,
+        method: "GET"
+      }).then(function(response) {
+        $("#mainCityUV").text(response.value);});
+      })
+}
